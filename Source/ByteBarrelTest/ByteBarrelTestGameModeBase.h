54b78718d772d10da9bff47be2512d8d4831c3bb
Nik <AoD.Nik@gmail.com>
2022-04-02 07:30:18 +0200

    Initial commit

---
+++ Source/ByteBarrelTest/ByteBarrelTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ByteBarrelTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BYTEBARRELTEST_API AByteBarrelTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
